1f9490332be9d07a50ee1e92700501725d8ed922
Antoine Lombardo <lombardoa@oxoc.net>
2022-12-22 00:04:05 -0500

    Missing files for standalone deployment.

--- scripts/standalone/instance-setup/setup.sh
+++ scripts/standalone/instance-setup/setup.sh
@@ -3,13 +3,39 @@
 source /scripts/common-setup-before.sh
 
 export APP_MODE=STANDALONE
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install mysql-server sysbench -y
+apt-get install mysql-server sysbench expect -y
 mkdir /shared
 curl https://downloads.mysql.com/docs/sakila-db.tar.gz --output /shared/sakila-db.tar.gz
 tar -xf /shared/sakila-db.tar.gz -C /shared
 rm /shared/sakila-db.tar.gz
 chmod -R 777 /shared
+
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'testpwd';"
+SECURE_MYSQL=$(expect -c "
+set timeout 10
+spawn mysql_secure_installation
+expect \"Enter password for user root:\"
+send \"testpwd\r\"
+expect \"Press y|Y for Yes, any other key for No:\"
+send \"n\r\"
+expect \"Change the password for root ?\"
+send \"n\r\"
+expect \"Remove anonymous users?\"
+send \"y\r\"
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+expect eof
+")
+echo "$SECURE_MYSQL"
+mysql -uroot -ptestpwd -e "DROP USER 'myapp'@'%';"
+mysql -uroot -ptestpwd -e "CREATE USER 'myapp'@'%' IDENTIFIED BY 'testpwd';"
+mysql -uroot -ptestpwd -e "GRANT ALL PRIVILEGES ON *.* TO 'myapp'@'%';"
 
 source /scripts/common-setup-after.sh
